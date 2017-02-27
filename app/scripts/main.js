31d2f267cb34c5f6a5c1359690f008927393f2b0
fsarachu <fsarachu@gmail.com>
2017-02-27 06:17:23 -0300

    Setup pickadate js plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('\'Allo \'Allo!');
+// Pickadate options
+var pickadateOptions = {
+  monthsFull: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ],
+  monthsShort: [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic'
+  ],
+  weekdaysFull: [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado'
+  ],
+  weekdaysShort: [
+    'Dom',
+    'Lun',
+    'Mar',
+    'Mié',
+    'Jue',
+    'Vie',
+    'Sáb'
+  ],
+}
+
+$('.datepicker').pickadate(pickadateOptions);
