b9cf42bdf8241ed0799abf2ddc4c8f3835adb879
fsarachu <fsarachu@gmail.com>
2017-03-06 11:27:15 -0300

    Remove "html" task

--- gulpfile.js
+++ gulpfile.js
@@ -42,25 +42,6 @@ gulp.task('scripts', () => {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('html', ['styles', 'scripts'], () => {
-  // TODO: Concatenate and minify assets in other tasks, remove HTML dependency
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    .pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: {compress: {drop_console: true}},
-      processConditionalComments: true,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
-    })))
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('images', () => {
   return gulp.src('app/resources/assets/images/**/*')
     .pipe($.cache($.imagemin()))
