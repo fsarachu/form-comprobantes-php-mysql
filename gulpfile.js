3bcc783fa8eb305d6051f8634f67a64778a8fc4a
fsarachu <fsarachu@gmail.com>
2017-03-06 11:31:27 -0300

    Update "fonts" task

--- gulpfile.js
+++ gulpfile.js
@@ -51,8 +51,8 @@ gulp.task('images', () => {
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {
   })
-    .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
+    .concat('app/resources/assets/fonts/**/*'))
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('clean', del.bind(null, [
