83d4087bd58de60e19246998b63cf70a5add6f66
fsarachu <fsarachu@gmail.com>
2017-03-07 00:10:18 -0300

    Add abstract Model class

---
+++ app/Database/Model.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+
+abstract class Model
+{
+  private static $table = null;
+  private static $db = null;
+  protected $exists = null;
+
+  public function __construct()
+  {
+    if (!$this->db)
+      $pdo_options = array(
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+        PDO::ATTR_PERSISTENT => true
+      );
+
+    $this->db = new PDO(
+      DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET,
+      DB_USER,
+      DB_PASS,
+      $pdo_options
+    );
+  }
+
+  abstract public function save();
+
+  abstract public function delete();
+
+  public static function all()
+  {
+    $sql = "SELECT * FROM " . static::$table;
+    $query = static::$db->prepare($sql);
+    $query->execute();
+
+    return $query->fetchAll();
+  }
+
+  public static function get($id)
+  {
+    $sql = "SELECT * FROM" . static::$table . "WHERE id = :id LIMIT 1";
+    $query = static::$db->prepare($sql);
+    $parameters = array(':id' => $id);
+    $query->execute($parameters);
+
+    return $query->fetch();
+  }
+}
