d6b2966569a997f68f16d7fb3c15694701de6dee
fsarachu <fsarachu@gmail.com>
2017-03-07 13:08:23 -0300

    Scaffold store() method of InvoiceController, which handles a new invoice POST request

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Core\Controller;
 use App\Models\Currency;
+use App\Models\Invoice;
 use App\Models\PaymentMethod;
 
 class InvoiceController extends Controller
@@ -25,8 +26,48 @@ class InvoiceController extends Controller
   {
     $_SESSION['default_currency'] = $_POST['currency'];
     $_SESSION['default_payment_method'] = $_POST['payment_method'];
+
     echo '<pre>';
     print_r($_POST);
     echo '</pre>';
+
+    $invoice = new Invoice();
+
+    if (isset($_POST['invoice_date'])) {
+      $invoice->setInvoiceDate($_POST['invoice_date']);
+    }
+
+    if (isset($_POST['payment_method'])) {
+      $invoice->setPaymentMethod($_POST['payment_method']);
+    }
+
+    if (isset($_POST['currency'])) {
+      $invoice->setCurrency($_POST['currency']);
+    }
+
+    if (isset($_POST['amount'])) {
+      $invoice->setAmount($_POST['amount']);
+    }
+
+    if (isset($_POST['description'])) {
+      $invoice->setDescription($_POST['description']);
+    }
+
+    if (isset($_POST['signed_by_business']) && $_POST['signed_by_business'] === 'true') {
+      $invoice->setSignedByBusiness(true);
+    }
+
+    // TODO: Image upload
+
+    try {
+      $invoice->save();
+      // TODO: Flash success message
+      static::redirect(BASE_URL . 'invoice/all');
+    } catch (Exception $e) {
+      // TODO: Flash error message
+      static::redirect(BASE_URL . 'invoice/new');
+    }
+
+
   }
 }
