babc99598126bd1de26c68c3402d62b5a79da775
fsarachu <fsarachu@gmail.com>
2017-03-07 01:45:08 -0300

    Add ConnectionFactory class

---
+++ app/Database/ConnectionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Database;
+
+
+class ConnectionFactory
+{
+  private static $pdo_options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::ATTR_PERSISTENT => true
+  ];
+
+  public static function create()
+  {
+    return new PDO(
+      DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET,
+      DB_USER,
+      DB_PASS,
+      self::$pdo_options
+    );
+  }
+}
