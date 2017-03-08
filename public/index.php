123075e534060dceca36f286f96dbae0d8e20629
fsarachu <fsarachu@gmail.com>
2017-03-08 01:32:00 -0300

    Test Invoice validator

--- public/index.php
+++ public/index.php
@@ -33,4 +33,37 @@ $router->get('/invoice/all', function () {
   InvoiceController::all();
 });
 
+$router->get('/validate', function () {
+  $obj = new \App\Models\Invoice();
+
+  echo '<h5>Before Set</h5>';
+  echo '<pre>';
+  print_r($obj);
+  echo '</pre>';
+
+  $obj->setInvoiceDate('2017-02-28');
+  $obj->setPaymentMethod('2');
+  $obj->setCurrency('4');
+  $obj->setAmount('11.00');
+  $obj->setDescription('ld,samdklasdklasm kdlasm dmaskl mdaskdlasm ldkasm dklsa');
+  $obj->setSignedByBusiness('true');
+
+  echo '<h5>After Set</h5>';
+  echo '<pre>';
+  print_r($obj);
+  echo '</pre>';
+
+  echo '<h5>Validate</h5>';
+  echo '<pre>';
+  print_r($obj->validate());
+  echo '</pre>';
+
+  echo '<h5>After Validate and Sanitize</h5>';
+  echo '<pre>';
+  print_r($obj);
+  echo '</pre>';
+
+
+});
+
 $router->run();
