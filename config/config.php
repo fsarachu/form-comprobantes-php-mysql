8d694b3f5dffbfc95babd3f6ef0c05e31b388971
fsarachu <fsarachu@gmail.com>
2017-03-08 17:23:51 -0300

    Reformat file

--- config/config.php
+++ config/config.php
@@ -4,16 +4,16 @@ define('UPLOAD_DIR', 'public/uploads');
 define('UPLOAD_URL', 'uploads/');
 define('ENVIRONMENT', 'development');
 
-if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
-  error_reporting(E_ALL);
-  ini_set("display_errors", 1);
-}
-
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'comprobantes_app');
 define('DB_USER', 'comprobantes_app');
 define('DB_PASS', 'secret');
 define('DB_CHARSET', 'utf8');
+
+if (ENVIRONMENT == 'development' || ENVIRONMENT == 'dev') {
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+}
 
 session_start();
